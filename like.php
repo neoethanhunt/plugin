f62adc1e4845f98f76eba8d4917829c230b44b1c
neoethanhunt <vamsikrishna.vaddempudi@my.jcu.edu.au>
2018-09-27 23:36:48 +1000

    plugin

---
+++ like.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: courselike
+
+
+
+*/
+
+/***************************
+* constants
+***************************/
+
+if(!defined('LI_BASE_DIR')) {
+	define('LI_BASE_DIR', dirname(__FILE__));
+}
+if(!defined('LI_BASE_URL')) {
+	define('LI_BASE_URL', plugin_dir_url(__FILE__));
+}
+
+
+/***************************
+* language files
+***************************/
+function li_load_text_domain() {
+	load_plugin_textdomain( 'like_it', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+}
+add_action( 'init', 'li_load_text_domain' );
+
+/***************************
+* includes
+***************************/
+include(LI_BASE_DIR . '/includes/display-functions.php');
+include(LI_BASE_DIR . '/includes/like.php');
+include(LI_BASE_DIR . '/includes/scripts.php');
+include(LI_BASE_DIR . '/includes/widgets.php');
---
+++ readme.txt
@@ -0,0 +1,11 @@
+=== like It ===
+
+
+
+
+
+== Description ==
+
+
+
+like It is a simple plugin that adds a "like It" link to your posts, pages, and custom post types.
